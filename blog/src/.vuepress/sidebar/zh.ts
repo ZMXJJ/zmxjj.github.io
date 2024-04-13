ba5ce0c58d34870d36bb5d65eed40543f62ade1c
ZMXJJ <huangsiyuan20060408@hotmail.com>
2024-04-13 19:59:29 +0800

    Language Texture Fix

--- blog/src/.vuepress/sidebar/zh.ts
+++ blog/src/.vuepress/sidebar/zh.ts
@@ -3,13 +3,13 @@ import { sidebar } from "vuepress-theme-hope";
 export const zhSidebar = sidebar({
   "/zh/": [
     "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
+    // {
+    //   text: "如何使用",
+    //   icon: "laptop-code",
+    //   prefix: "demo/",
+    //   link: "demo/",
+    //   children: "structure",
+    // },
     {
       text: "文章",
       icon: "book",
@@ -17,10 +17,10 @@ export const zhSidebar = sidebar({
       children: "structure",
     },
     "intro",
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
-    },
+    // {
+    //   text: "幻灯片",
+    //   icon: "person-chalkboard",
+    //   link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
+    // },
   ],
 });
