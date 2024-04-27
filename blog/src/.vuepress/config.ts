a8639c5d728e2d6662bca061a6dff6691b735fef
Dennis Huang <45286771+ZMXJJ@users.noreply.github.com>
2024-04-27 04:50:39 +0000

    Fonts fix

--- blog/src/.vuepress/config.ts
+++ blog/src/.vuepress/config.ts
@@ -21,4 +21,42 @@ export default defineUserConfig({
 
   // Enable it with pwa
   // shouldPrefetch: false,
+
+  //import fonts
+  head: [
+
+    // 导入字体
+    [
+      "link",
+      {
+        href: "https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Regular.min.css",
+        rel: "stylesheet",
+        crossorigin: "anonymous",
+      },
+    ],
+    [
+      "link",
+      {
+        href: "https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Normal.min.css",
+        rel: "stylesheet",
+        crossorigin: "anonymous",
+      },
+    ],
+    [
+      "link",
+      {
+        href: "https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Bold.min.css",
+        rel: "stylesheet",
+        crossorigin: "anonymous",
+      },
+    ],
+    [
+      "link",
+      {
+        href: "https://cdn.jsdelivr.net/npm/misans@4.0.0/lib/Normal/MiSans-Medium.min.css",
+        rel: "stylesheet",
+        crossorigin: "anonymous",
+      },
+    ],
+  ],
 });
